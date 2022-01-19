2ec13dc030ec8d361c9b784e59209447c7edc9fb
unknown <deonisius97@gmail.com>
2022-01-19 16:14:32 +0700

    edit usecase

--- usecase/usecase_user.go
+++ usecase/usecase_user.go
@@ -3,49 +3,24 @@ package usecase
 import (
 	"crud/entity"
 	"crud/repository"
-	"encoding/json"
-	"net/http"
-
-	"github.com/gorilla/mux"
 )
 
-func GetUsers(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-	var users []entity.User
-	repository.DB.Find(&users)
-	json.NewEncoder(w).Encode(users)
+func Get(user *[]entity.User) {
+	repository.DB.Find(user)
 }
 
-func GetUser(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-	params := mux.Vars(r)
-	var user entity.User
-	repository.DB.First(&user, params["id"])
-	json.NewEncoder(w).Encode(user)
+func GetId(users *entity.User, id string) {
+	repository.DB.First(users, id)
 }
 
-func CreateUser(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-	var user entity.User
-	json.NewDecoder(r.Body).Decode(&user)
-	repository.DB.Create(&user)
-	json.NewEncoder(w).Encode(user)
+func Create(user *entity.User) {
+	repository.DB.Create(user)
 }
 
-func UpdateUsers(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-	params := mux.Vars(r)
-	var user entity.User
-	repository.DB.First(&user, params["id"])
-	json.NewDecoder(r.Body).Decode(&user)
+func Update(user *entity.User) {
 	repository.DB.Save(&user)
-	json.NewEncoder(w).Encode(user)
 }
 
-func DeleteUser(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-	params := mux.Vars(r)
-	var user entity.User
-	repository.DB.Delete(&user, params["id"])
-	json.NewEncoder(w).Encode("data sukses terhapus")
+func Deleted(user *entity.User, id string) {
+	repository.DB.Delete(user, id)
 }
